281eb9d72ef9f46cbc19e0ec717ef7fad6f822a8
yuuOkada <y.okada.mt133@gmail.com>
2019-12-12 19:17:32 +0900

    191212 - fixed Cam.py

--- Cam.py
+++ Cam.py
@@ -1,8 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
 import time
+import datetime
+import numpy as np
+import cv2
 import picamera
 
-with picamera.Picamera() as cam:
-    cam.resolution(2400, 1800)
-    cam.start_preview()
-    time.sleep(2)
-    cam.capture("1.jpg")
+i = 0
+
+while i < 5:
+    
+    now_time = datetime.datetime.now().strftime('%Y%m%d%H')
+    
+    filename = str(now_time) + "_" + str(i) + ".jpg"
+    
+    with picamera.PiCamera() as cam:
+        #cam.resolution()
+        cam.start_preview()
+        time.sleep(0.2)
+        cam.capture(filename)
+    
+    img = cv2.imread(filename)
+    rotated_img = cv2.rotate(img, cv2.ROTATE_180)
+    
+    cv2.imwrite(filename, rotated_img)
+    
+    time.sleep(0.5)
+    
+    i += 1
